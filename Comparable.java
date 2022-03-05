c94c26ce33adc930ae16b731d4380e6d52b444b5
ZacharyHinoteCollegeOTC <78001975+ZacharyHinoteCollegeOTC@users.noreply.github.com>
2022-03-04 19:13:17 -0600

    Create Comparable.java

---
+++ Comparable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Zachary Hinote
+ */
+//Interface for comparing objects. 
+public interface Comparable<E> {
+    public int compareTo(E o);
+}
